e6636986d75723f33ed278ff32b96546d0c7e295
prithivi111 <prithivi1808@gmail.com>
2024-01-08 00:10:17 -0600

    Finished learning

--- src/main/java/com/zorba/book/BookController.java
+++ src/main/java/com/zorba/book/BookController.java
@@ -1,23 +1,29 @@
 package com.zorba.book;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//@Controller //this is used in traditional MVC applications where the controller is responsible for handling requests and
-			  // and returning views.
+//@Controller //this is used in traditional MVC applications where the controller is responsible for handling requests and returning views.
 //public class BookController {
 	
 //	@RequestMapping(value="/books", method=RequestMethod.GET)
 //	@ResponseBody  //This will return the string as it is, mentioned in the getBooks() method, ie,"This is just testing for book",
-				// which the user will be veiwing on the web page.s it is. But, if we want to send response in JSON format
+				// which the user will be veiwing on the web page as it is. But, if we want to send response in JSON format
 				// rather than the rendered view,  we can change the @Controller to @RestController and we also don't need to
 				// mention @ResponseBody. Meaning that it combines the @Controller and @ResponseBody annotations, meaning that
 				//every method in a @RestController class retuns the data directly(not a view) in a foramt like JSON or XML.
@@ -35,19 +41,22 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 	
+	//Get one-book Handler (Assuming that there is only one book)
 //	@GetMapping("/books")           
 //	public BookEntity getBooks() {
 //		BookEntity bookInfo = bookService.saveBookInfo();
 //		return bookInfo;
 //	}	
 	
-	@GetMapping("/books")  //This is for getting all the books
-	public List<BookEntity> getAllBooks() {
-		List<BookEntity> listBooks = bookService.getAllBooks();
-		return listBooks;
-		
-	}
-	
+	//Get All books Handler
+//	@GetMapping("/books")  
+//	public List<BookEntity> getAllBooks() {
+//		List<BookEntity> listBooks = bookService.getAllBooks();
+//		return listBooks;
+//		
+//	}
+
+	//Get Single book handler with id in the parameter
 	@GetMapping("/books/{id}")
 	public BookEntity getBookById(@PathVariable("id") int id) {	//Must need to define path variable to bind this method
 		                                                       // with the input which user gives in the URL
@@ -55,6 +64,86 @@ public class BookController {
 		
 	}
 	
+	//New Book handler
+	@PostMapping("/books")  //Here we are writing Post instead of Get because we are adding book into the DB this time.
+	public BookEntity addBook(@RequestBody BookEntity book) {  
+		BookEntity b= this.bookService.addBook(book);
+		System.out.println(book);
+		return b;
+	}
+	//@RequestBody annotation indicates that the book parameter should be bound to the HTTP request body.
+	//@RequestBody annotation is crucial here because it tells Spring to 
+	//deserialize the incoming JSON (or other supported format) from the request body into a BookEntity object.
 	
+	//Delete Book Handler
+	@DeleteMapping("/books/{Bookkoid}")
+	public void deleteBook(@PathVariable("Bookkoid") int id) {
+		 this.bookService.deleteBook(id);
+		}
+	
+	//Update Book handler
+	@PutMapping("/books/{bookid}")
+	public BookEntity updateBook(@RequestBody BookEntity book, @PathVariable("bookid") int id) {
+		this.bookService.updateBook(book, id);
+			return book;
+	}
+	
+	//Get All Books Handler (implementation of codes handling HTTP response, assuming there might not be any data in DB)
+	//Here, we are trying to get the data from the our list(Database), and there is no data in the database.
+	//So we must send the something back to the User regarding Status so that User will get to know that there is nothing in DB.
+	//If we do not code this exception then, the UI will have blank on their screen! So, in reality, for getting the data,
+	//we need to write the code as below, which will include response regarding the Status of the User's Request as well.
+	//For doing this, lets comment-out our list of books in the Service Class and I commented-out.
+	//At the very top, we wrote the code somthing like below and the we get the list of books and we return back the list of
+	//books to the users and User will be able to see on their screen.
+//		@GetMapping("/books")
+//			public List<BookEntity> getAllBooks() {
+//				List<BookEntity> listBooks = bookService.getAllBooks();
+//					return listBooks;
+//			}
+//	
+	//But lets write in the different way handling the exception/status, if no book record is there in the DB.
+	//In the postman as well, you will see the status as 200 Ok, and there will be no data obviously,
+	
+	@GetMapping("/books")
+	public ResponseEntity<List<BookEntity>> getAllBooks() { 
+		//here we are sending back "Status not found" right, that is why the return type should be ResponseEntity.
+		//And ResponseEntity will have List<BookEntity>.		
+			
+			List<BookEntity> list = bookService.getAllBooks();
+				if(list.size()<=0) {
+			
+					return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+					//ResponseEntity contains the status of NOT_FOUND and the object of ResponseEntity will be created by build().
+				}else {
+					return ResponseEntity.of(Optional.of(list));
+					//or we can write: 
+					//return ResponseEntity.ok(list);
+					//ResponseEntity.ok() is a static factory method provided by the ResponseEntity class in Spring.
+					//It creates a ResponseEntity object that will provide an HTTP status of 200 OK.
+				}	
+	
+	//ResponseEntity is a class in Spring Framework that represents the entire HTTP response. It allows you to control
+	//the HTTP status code, headers, and body of the response.
+	
+	//HttpStatus.NOT_FOUND: HttpStatus is an enumeration in Spring that represents HTTP status codes. 
+	//NOT_FOUND is a constant representing the 404 status code,which indicates that the requested resource is not found
+    //on the server.
+	
+	//.build():	It's a method of the ResponseEntity class used to build and create the final response.
+    //In this context, it's used to create a response(object) with the specified HTTP status code and no response body.
+	
+	//Optional: Optional is class that belongs to java.util package, and it has seveeral methods. It is a container object
+	//that may or may not contain a non-null value. It helps handle scenarios
+	//where a value might be present or absent without explicitly using null. It has methods, which are used for more
+//expressive an null-safe code, especlly in cases where a value might be absent and Optional.of(value) is one of those methods.
+	
+	//Optional.of(list) is a static method of the Optional Class that creates an Optional object containing the provided list.
+	//Even if the list is empty, it will still be wrapped in an Optional.
+				
+	//ResponseEntity.of(...): The .of method is an extension method provided by Spring for the ResponseEntity class.
+	//It creates a new ResponseEntity with the specified body and status. 
+	//In the above case, the body is an Optional containing the list.
+	}
 	
 }
--- src/main/java/com/zorba/book/BookService.java
+++ src/main/java/com/zorba/book/BookService.java
@@ -2,14 +2,15 @@ package com.zorba.book;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
-	//	This is a suppose, think as if a data base data for the time being.
+	//	Think as if the values below I provided, are the values of the data base data for the time being.
 	//We will later join the Spring Data JPA to link actually fetch and store the data from/into the database,
-	//where we will be further using one more layer called Repository!!
+	//where we will be further using one more layer called Repository, that we have alreay learned before!
 	
 //	public BookEntity saveBookInfo() {
 //		BookEntity bookEntity = new BookEntity();
@@ -19,35 +20,75 @@ public class BookService {
 //		return bookEntity;
 //		}
 	
-	//Instead, we are making a fake database to do similar-like-database operations.
-	//Lets make list of books in this class itself.
-	//If we had not made the below database in this class, we need to make another layer to connect to the database called
+	//Lets make list of books in this class itself, and consider as the datas of the database again.
+	//If we had not made the below database values in this class, we need to make another layer to connect to the database called
 	//Repository Layer that we learned in Spring Data JPA.
 		
-		private static List<BookEntity> list = new ArrayList<>();
-		
+		private static List<BookEntity> list = new ArrayList<>();	
 		{
-			list.add(new BookEntity(12,"Alibaba and Chalis Chor", "Ramesh"));
-			list.add(new BookEntity(22, "Life in Japan", "Yoshida"));
-			list.add(new BookEntity(998,"Newyork Night", "Simon"));
+//			list.add(new BookEntity(12,"Alibaba and Chalis Chor", "Ramesh"));
+//			list.add(new BookEntity(22, "Life in Japan", "Yoshida"));
+//			list.add(new BookEntity(998,"Newyork Night", "Simon"));
 		}
 		
-		//get all books
+		//return all books to the User who is fetching via browser
 		public List<BookEntity> getAllBooks(){
 			return list;
+			
 		}
 	
-		//get single book by Id
+		//return a single book searching by Id by the User who is fetching via browser
 		public BookEntity getBookById(int id) {  
 			BookEntity bookMatch= null;
 			for(BookEntity book:list) {
 				if (book.getId()==id){
 					bookMatch = book;
 					break;
-				}
-				
+				}				
 			}
 			return bookMatch;
 		}
+		
+		//adding the book in our DB as per the information provided by the user via browser
+		public BookEntity addBook(BookEntity book) {
+			list.add(book);
+			return book;
+		}
+		
+		//delete the book from our DB as per the information (id)
+		public void deleteBook(int id) {
+			list=list.stream().filter(book->{
+				if(book.getId()!=id) {
+					return true;
+				}else {
+					return false;
+				}			
+			}).collect(Collectors.toList());		
+			//We can write this above code in simple form.
+			//We just filtered our list where we used lamda expression and stream API. The code might be little confusing for you.
+		}
+		
+		//update the book in our DB as per the 
+		public void updateBook(BookEntity book, int bookId) {
+//			list = list.stream().map(e->{
+//				if(e.getId()==bookId) {
+//					e.setTitle(book.getTitle());
+//					e.setAuthor(book.getAuthor());
+//				}
+//				return e;
+//			}).collect(Collectors.toList());
+//		
+//		
+		//The above code is equivalent to:
+			for (int i = 0; i < list.size(); i++) {
+			  BookEntity e = list.get(i);
+			    if (e.getId() == bookId) {
+			          e.setTitle(book.getTitle());
+			          e.setAuthor(book.getAuthor());
+			          break;
+			        }
+			 }
+			
+	}	
 }
 	
